51268d3c382bc238dc78638e00a7da71ceae4398
Nikita Bulavinov <ns.bulavinov@gmail.com>
2024-07-25 15:28:56 +0300

    feat: Add LevelFilter

--- src/logging_extension/__init__.py
+++ src/logging_extension/__init__.py
@@ -1,11 +1,12 @@
 from .formatters import JSONFormatter
 from .handlers import ThreadedHandler
-from .filters import BelowLevelFilter
+from .filters import BelowLevelFilter, LevelFilter
 
 
 __all__ = (
     JSONFormatter,
     ThreadedHandler,
     BelowLevelFilter,
+    LevelFilter,
 )
-__version__ = "0.2.0"
+__version__ = "0.3.0"
